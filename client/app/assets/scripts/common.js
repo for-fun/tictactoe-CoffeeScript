bb448b6add34a59de1dd3948084937b7fc8c8410
AveVlad <co1dp1ay@ya.ru>
2013-10-18 23:51:48 +0400

    update game class

--- client/app/assets/scripts/common.js
+++ client/app/assets/scripts/common.js
@@ -2,6 +2,16 @@
   var Game, go;
 
   Game = (function() {
+    var elem, elemX, elemZero, winningArray;
+
+    elem = true;
+
+    elemZero = 0;
+
+    elemX = 0;
+
+    winningArray = [123, 456, 789, 147, 258, 369, 159, 359];
+
     function Game() {
       this.addElement();
       this.clickElement();
@@ -12,8 +22,7 @@
       gameWindow = $('.window');
       _results = [];
       for (i = _i = 1; _i <= 9; i = ++_i) {
-        gameWindow.append('<li class="item" data-id=' + i + '></li>');
-        _results.push(console.log(i));
+        _results.push(gameWindow.append('<li class="item" data-id=' + i + '></li>'));
       }
       return _results;
     };
@@ -24,9 +33,51 @@
       return gameWindowLi.click(function() {
         var $this;
         $this = $(this);
-        $this.addClass('zero');
-        return console.log(123);
+        if ($this.hasClass('zero') === true || $this.hasClass('x') === true) {
+          return false;
+        }
+        if (elem) {
+          $this.addClass('zero');
+          elemZero++;
+          elem = false;
+        } else {
+          $this.addClass('x');
+          elemX++;
+          elem = true;
+        }
+        if (elemZero + elemX >= 5) {
+          return Game.prototype.checkWinning.call(this);
+        }
       });
+    };
+
+    Game.prototype.checkWinning = function(li) {
+      var checker, elementsNumber, i, x, xArr, zero, zeroArr, _i;
+      this.li = li;
+      zeroArr = [];
+      xArr = [];
+      li = $('.window .item');
+      for (i = _i = 0; _i <= 8; i = ++_i) {
+        if (li.eq(i).hasClass('zero')) {
+          zero = i + 1;
+          zeroArr.push(zero);
+        }
+        if (li.eq(i).hasClass('x')) {
+          x = i + 1;
+          xArr.push(x);
+        }
+      }
+      console.log('zero', zeroArr);
+      console.log('x', xArr);
+      elementsNumber = 0;
+      checker = function(mainStr, checkStr) {
+        mainStr = mainStr.split('');
+        checkStr = checkStr.split('');
+        return mainStr.split('').reduce(function(a, b) {
+          return a.replace(RegExp(b), '');
+        }, checkStr).length === 0;
+      };
+      return true;
     };
 
     return Game;
--- client/app/coffee/Game.coffee
+++ client/app/coffee/Game.coffee
@@ -1,4 +1,21 @@
 class Game
+    elem = true
+    elemZero = 0
+    elemX = 0
+
+    winningArray = [
+        # по горизонтали
+        123,
+        456,
+        789,
+        # по вертикали
+        147,
+        258,
+        369,
+        # по диагонали
+        159,
+        359,
+    ]
 
     constructor: () ->
         @addElement()
@@ -9,13 +26,50 @@ class Game
 
         for i in [1..9]
             gameWindow.append('<li class="item" data-id=' + i + '></li>')
-            console.log i
 
     clickElement: () ->
         gameWindowLi = $('.window .item')
         gameWindowLi.click ->
             $this = $(this)
-            $this.addClass('zero')
-            console.log 123
+            if ($this.hasClass('zero') == true || $this.hasClass('x') == true)
+                return false
+            if (elem)
+                $this.addClass('zero')
+                elemZero++
+                elem = false
+            else
+                $this.addClass('x')
+                elemX++
+                elem = true
+            if (elemZero + elemX >= 5)
+                Game.prototype.checkWinning.call(this);
+
+    checkWinning: (@li) ->
+        zeroArr = []
+        xArr = []
+        li = $('.window .item')
+
+        for i in [0..8]
+            if (li.eq(i).hasClass('zero'))
+                zero = i + 1
+                zeroArr.push(zero)
+            if (li.eq(i).hasClass('x'))
+                x = i + 1
+                xArr.push(x)
+
+        console.log 'zero', zeroArr
+        console.log 'x', xArr
+
+
+        elementsNumber = 0
+
+        checker = (mainStr, checkStr) ->
+            mainStr = mainStr.split('')
+            checkStr = checkStr.split('')
+            return mainStr.split('').reduce( (a, b) ->
+                return a.replace(RegExp(b), '')
+            , checkStr).length == 0
+
+        return true
 
 go = new Game
